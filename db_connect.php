cdbb76cb3d33da18bd14f74388270955ae444084
Niveditha R <72128427+nived12345@users.noreply.github.com>
2020-11-21 22:27:06 +0530

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,6 @@
+<?php 
+	$conn=mysqli_connect("localhost", "id15463569_root", "Niveditha12,.", "id15463569_bank");
+	if(!$conn) {
+		echo "Connection Error: " . mysqli_connect_error();
+	}
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Banking System</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <?php include('navbar.php'); ?>
+
+    <section>
+        <div class="container">
+        <div class="row">
+            <div class="col s6 description" style="margin-top:250px">
+                <p><h7><b>Welcome to
+                Simple Banking System</b></h7>
+                </p>
+            </div>
+            <div class="col s6">
+                <img src="Bank1.jpg" class="vector" width="550px" height="550px">
+            </div>
+        
+        <div class="go-to">
+        	<a href="transferHistory.php" class="waves-effect waves-light btn-large black z-depth-2">TRANSFER DETAILS</a>
+        	<a href="selectUser.php" class="waves-effect waves-light btn-large black z-depth-2">SELECT CUSTOMERS</a>
+        </div>
+    </div>
+    
+    </section>
+
+</body>
+
+</html>
